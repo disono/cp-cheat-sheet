02a26d255989d514f6428a23cc1a26181716056c
Archie Disono <disono@users.noreply.github.com>
2020-10-25 22:38:45 +0800

    2D vector

--- essential/main.cpp
+++ essential/main.cpp
@@ -4,28 +4,62 @@
 using namespace std;
 
 int main() {
-    vector<int> g1;
+    // simple vector
+    cout << "Simple Vector" << endl;
+    vector<int> a = {0, 1, 2, 3, 4};
+    for (size_t i = 0; i < a.size(); i++) {
+        std::cout << a[i] << " ";
+    }
+    cout << endl;
 
-    for (int i = 1; i <= 5; i++)
-        g1.push_back(i);
-    g1.push_back(100);
-    g1.push_back(75);
+    // 2d vector
+    // vector of vector
+    cout << "2D Vector" << endl;
+    vector<vector<int>> vt{
+            {1, 2},
+            {3, 4, 5, 6}
+    };
 
-    cout << "Output of begin and end: ";
-    for (auto i = g1.begin(); i != g1.end(); ++i)
-        cout << *i << " ";
+    for (int i = 0; i < vt.size(); i++) {
+        for (int j = 0; j < vt[i].size(); j++) {
+            cout << vt[i][j] << ", ";
+        }
 
-    cout << "\nOutput of cbegin and cend: ";
-    for (auto i = g1.cbegin(); i != g1.cend(); ++i)
-        cout << *i << " ";
+        cout << endl;
+    }
 
-    cout << "\nOutput of rbegin and rend: ";
-    for (auto ir = g1.rbegin(); ir != g1.rend(); ++ir)
-        cout << *ir << " ";
+    // row & column
+    // define how many rows the 2d vector is going to have
+    cout << "Create a 2D Vector" << endl;
+    int row = 5;
 
-    cout << "\nOutput of crbegin and crend : ";
-    for (auto ir = g1.crbegin(); ir != g1.crend(); ++ir)
-        cout << *ir << " ";
+    // define the number of values each row to have
+    int column[] = {5, 10, 15, 20, 25};
+
+    // create a vector of vector with size equal to row
+    vector<vector<int>> vec(row);
+
+    // now assign structure of 2d vector by assigning the value
+    // of rows and columns through a nested for loop
+    for (int i = 0; i < row; i++) {
+        // declare the size of the column
+        int col = column[i];
+
+        // define each value in row
+        vec[i] = vector<int>(col);
+        for (int j = 0; j < col; j++) {
+            vec[i][j] = j + 1;
+        }
+    }
+
+    // now print the 2d vector created above
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < vec[i].size(); j++) {
+            cout << vec[i][j] << ", ";
+        }
+
+        cout << endl;
+    }
 
     return 0;
 }
